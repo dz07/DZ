abdb012ba581a8014f695b4b522f4c09dd06e28c
dz07 <33198285+dz07@users.noreply.github.com>
2018-10-13 13:46:50 +0100

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,5 +1,5 @@
 cd ~/Desktop
 rm -rf ~/Desktop/mybanana
-git clone https://github.com/dz07/DZ.git i8
-cd ~/Desktop/i8/Contents/MacOS
-./Banana
+git clone https://github.com/dz07/DZ.git 
+cd ~/Desktop/mybanana
+./run
